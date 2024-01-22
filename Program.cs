a1fa99bfb25093b32708c00f6ad3f73c2156d9d7
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-22 12:20:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration
+            string name = "Nondumiso";
+            int age =23;
+            bool isTall =false;
+            double weight =110;
+
+            /*for the smalleat least precise decimal use float,
+             * for more precise use decimal eg for money,
+             * in most cases double will do just fine
+             * */
+
+            Console.WriteLine(name + "is" + age + " she weighs" + weight);
+            Console.WriteLine("it is" + isTall + "that he is tall");
+
+            //freeze console 
+            Console.ReadLine();
+
+
+        }
+    }
+}
